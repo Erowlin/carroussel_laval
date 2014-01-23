1d774f1f72706af9a7ad8d4deb747cac91b1c861
Thomas Romera <erowlin@Thomass-MacBook-Air.local>
2014-01-23 18:51:31 -0500

    Ok

--- res/js/index.js
+++ res/js/index.js
@@ -2,26 +2,23 @@ window.onload=function(){
 
 	var step = 1;
 	var max_step = 2;
+
+	var get_step = function (action) {
+		if (action == 0) // left
+			step = (step == 1 ? (max_step) : (step - 1));
+		else
+			step = (step == max_step ? (1) : (step + 1));
+		return step;
+	};
+
 	// Mon code javascript après que la page se soit chargée
 	document.getElementById('previous').onclick=function(){
-		if (step == 1) { // On doit aller à la dernière image
-			step = max_step
-		}
-		else {
-			--step;
-		}
-		document.getElementById('image').src = "res/img/img"+step+".jpg";
+		document.getElementById('image').src = "res/img/img" + get_step(0) + ".jpg";
 	};
 
 	document.getElementById('next').onclick=function(){
-		if (step == max_step) { // On doit aller à la dernière image
-			step = 1;
-		}
-		else {
-			++step;
-		}
-			document.getElementById('image').src = "res/img/img"+step+".jpg";
+		document.getElementById('image').src = "res/img/img"+get_step(1) +".jpg";
 	};
 
-
+	window.setInterval("javascript function",milliseconds);
 };
