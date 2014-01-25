5e5023b8dd3b915c7e1d7e1fc88fd5b19e82574b
Thomas Romera <erowlin@Thomass-MacBook-Air.local>
2014-01-24 19:01:31 -0500

    Nouvelle version du labo

--- res/js/index.js
+++ res/js/index.js
@@ -1,24 +1,50 @@
 window.onload=function(){
+	
+	var time = 0;
+	
+	// Get all the images with the carrousel-image class
+	var getImages = function(){
+		return document.getElementsByClassName("carrousel-image");
+	}
 
-	var step = 1;
-	var max_step = 2;
+	// Change current active image
+	// State = 0 -> Previous
+	// State = 1 -> Next
+	var changeActiveImage = function(state) {
+			time = 0;
+			var images = getImages();
+			var step = 0;
+			var changed = false;
+			Array.prototype.forEach.call(images, function(el){
+				if (changed == false && el.className.indexOf("active") >= 0) // Active trouvé dans la balise
+				{
+					el.className = "carrousel-image"
+					if ( state == 0 ) // Previous 
+						(step != 0 ) ? (step--) : (step = (images.length - 1)); // Do you like ternaire ?
+					else // Next
+						(step != images.length - 1) ? (step++) : (step = 0);
+					images[step].className = "carrousel-image active";
+					changed = true; // Trick because break or return suck in forEach;
+				}
+				step++;
+			});
+	}
 
-	var get_step = function (action) {
-		if (action == 0) // left
-			step = (step == 1 ? (max_step) : (step - 1));
-		else
-			step = (step == max_step ? (1) : (step + 1));
-		return step;
-	};
-
-	// Mon code javascript après que la page se soit chargée
 	document.getElementById('previous').onclick=function(){
-		document.getElementById('image').src = "res/img/img" + get_step(0) + ".jpg";
+		changeActiveImage(0);
 	};
 
 	document.getElementById('next').onclick=function(){
-		document.getElementById('image').src = "res/img/img"+get_step(1) +".jpg";
+		changeActiveImage(1);
 	};
 
-	window.setInterval("javascript function",milliseconds);
+	// Timer for image rotation
+	var timer = function () {
+		if (time > 4)
+			changeActiveImage(1);
+		else
+			time++;
+	};
+
+	window.setInterval(timer, 1000);
 };
